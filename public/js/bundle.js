ef6c537b9ab5986415212c8e53f30b7c8b14eafd
Daniel Udofia <udofiadaniel07@gmail.com>
2025-06-05 16:00:36 +0100

    Bundled the js file

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -13093,7 +13093,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65269" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55559" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
